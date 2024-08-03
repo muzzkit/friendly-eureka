8c0ecd11e5c4dcdf3bc3ea344fabbd85c5e6fcd6
muiz azeez <66578279+muzzkit@users.noreply.github.com>
2024-08-03 17:48:29 +0100

    Update index.ts

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,3 @@
-//AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU
-//<reference types="@types/google.maps"/>
 import { User } from './user';
 import { Company } from './Company';
 import { CustomMap } from './CustomMap'
